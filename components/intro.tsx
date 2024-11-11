bdb289424ad2815b036e3a0796a3cb64d18b15da
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-11 20:00:06 +0000

    profile picture styled into the intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,17 +5,23 @@ export default function Intro() {
   return (
     <section>
        <div className="flex items-center justify-center">
-         <div>
+         <div className="relative">
             <Image 
-            src="/profile-picture.jpg"
-            alt="profile"
-            width="192"
-            height="192"
-            quality="95"
-            priority={true} 
+              src="/profile-picture.jpg"
+              alt="coste ionut profile-picture"
+              width="292"
+              height="292"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover 
+              border-[0.01rem] border-white shadow-xl"
             />
+
+            <span className="absolute text-4xl bottom-0 right-0">
+                👋
+            </span>
          </div>
-       </div> 
+       </div>
     </section>
   )
 }
