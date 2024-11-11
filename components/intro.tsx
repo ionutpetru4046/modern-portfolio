c6d67166fcce99e4b0c55f9183ee5dd849511b63
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-11 21:30:54 +0000

    links added to the intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -56,6 +60,41 @@ export default function Intro() {
               sites & apps </span>. My Focus is{"  "}
               <span className="underline">React (Next.js)</span>.
        </motion.p>
+
+       <motion.div className="flex flex-col sm:flex-row items-center justify-center 
+       gap-2 px-4 text-lg font-medium"
+       initial={{ opacity: 0, y: 100 }}
+       animate={{ opacity: 1, y: 0 }}
+       transition={{
+        delay: 0.1,
+       }}
+       >
+        <Link 
+        href="#contact" 
+        className="group bg-gray-900 text-white px-7 py-3 
+        flex items-center gap-2 rounded-full outline-none 
+        focus:scale-110 hover:scale-110 hover:bg-gray-950 
+        active:scale-105 transition">
+        Contact Me Here <BsArrowRight className="opacity-70 
+        group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a className="group bg-white px-7 py-3 flex 
+        items-center gap-2 rounded-full outline-none focus:scale-110 
+        hover:scale-110 active:scale-105 transition cursor-pointer" href="/Resume.pdf" download>
+          Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          </a>
+
+          <a className="bg-white p-4 text-gray-700 flex items-center 
+          gap-2 rounded-full">
+              <BsLinkedin />
+          </a>
+
+          <a className="bg-white p-4 text-gray-700 flex items-center 
+          gap-2 text-[1.25rem] rounded-full">
+              <FaGithubSquare />
+          </a>
+       </motion.div>
     </section>
   )
 }
