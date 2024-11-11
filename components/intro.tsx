8986d320b1b1dca7e25500643712229085b5c394
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-11 23:07:45 +0000

    about component finished

--- components/intro.tsx
+++ components/intro.tsx
@@ -47,7 +47,7 @@ export default function Intro() {
          </div>
        </div>
 
-       <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+       <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
        >
@@ -59,7 +59,7 @@ export default function Intro() {
              of experience. I enjoy building <span className="italic">
               sites & apps </span>. My Focus is{"  "}
               <span className="underline">React (Next.js)</span>.
-       </motion.p>
+       </motion.h1>
 
        <motion.div className="flex flex-col sm:flex-row items-center justify-center 
        gap-2 px-4 text-lg font-medium"
