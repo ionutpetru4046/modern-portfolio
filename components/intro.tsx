d6c374b8f108860d80319befe31a60634fbeadab
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-11 19:43:31 +0000

    image added to the intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import React from "react";
+
 export default function Intro() {
   return (
     <section>
-      
+       <div className="flex items-center justify-center">
+         <div>
+            <Image 
+            src="/profile-picture.jpg"
+            alt="profile"
+            width="192"
+            height="192"
+            quality="95"
+            priority={true} 
+            />
+         </div>
+       </div> 
     </section>
   )
 }
