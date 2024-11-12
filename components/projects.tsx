cb1556717d2057edbedd462b8b91803f65cb9f18
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-12 10:56:13 +0000

    changes added to the styled section of project component

--- components/projects.tsx
+++ components/projects.tsx
@@ -24,8 +24,8 @@ function Project({ title, description, tags, imageUrl }:
 ProjectProps) {
     return <section 
     className='bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden 
-    sm:pr-8 relative sm:h-[20rem]'>
-        <div className='py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] 
+    sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0'>
+        <div className='pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] 
         flex flex-col h-full'>
             <h3 
             className='text-2xl font-semibold'>{title}</h3>
