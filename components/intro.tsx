c4c4cabeda0ce159d55f2532ce32b73f60c0d5e7
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-12 20:28:45 +0000

    changes made to .hintrc file into the project intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -23,7 +23,11 @@ export default function Intro() {
   }, [inView, setActiveSection]);
 
   return (
-    <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
+    <section 
+    ref={ref} 
+    id="home" 
+    className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
        <div className="flex items-center justify-center">
          <div className="relative">
           <motion.div
@@ -45,6 +49,7 @@ export default function Intro() {
               border-[0.01rem] border-white shadow-xl"
             />
           </motion.div>
+
             <motion.span className="absolute text-4xl bottom-0 right-0"
               initial={{ opacity: 0, scale: 0 }}
               animate={{ opacity: 1, scale: 1 }}
@@ -87,22 +92,28 @@ export default function Intro() {
         className="group bg-gray-900 text-white px-7 py-3 
         flex items-center gap-2 rounded-full outline-none 
         focus:scale-110 hover:scale-110 hover:bg-gray-950 
-        active:scale-105 transition">
-        Contact Me Here <BsArrowRight className="opacity-70 
+        active:scale-105 transition"
+        >
+        Contact Me Here{" "}
+        <BsArrowRight className="opacity-70 
         group-hover:translate-x-1 transition" />
         </Link>
 
-        <a className="group bg-white px-7 py-3 flex 
-        items-center gap-2 rounded-full outline-none focus:scale-110 
-        hover:scale-110 active:scale-105 transition cursor-pointer border 
-        border-black/10" href="/Resume.pdf" download>
-          Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        <a 
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" 
+          href="/Resume.pdf" download
+        >
+          Download CV{" "} 
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
           </a>
 
-          <a className="bg-white p-4 text-gray-700 flex 
+          <a 
+            className="bg-white p-4 text-gray-700 flex 
         items-center gap-2 rounded-full focus:scale-110 
         hover:scale-110 active:scale-105 hover:text-blue-600 transition cursor-pointer border 
-        border-black/10" href="https://www.linkedin.com/in/coste-ionut-7531312bb/" target="_blank" rel="noopener">
+        border-black/10" href="https://www.linkedin.com/in/coste-ionut-7531312bb/" 
+        target="_blank"
+        >
               <BsLinkedin />
           </a>
 
@@ -112,11 +123,10 @@ export default function Intro() {
              hover:scale-110 active:scale-105 hover:text-red-700 transition 
              cursor-pointer border border-black/10"
              href="https://github.com/ionutpetru4046" 
-             target="_blank" rel="noopener"
-          >
+             target="_blank">
             <FaGithubSquare />
           </a>
        </motion.div>
     </section>
-  )
+  );
 }
