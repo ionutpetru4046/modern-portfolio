39d10d36dbcc93ba838d9d76008dfa1d6fdb15d2
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-12 13:55:24 +0000

    project.tsx component finished added styling and animations

--- components/project.tsx
+++ components/project.tsx
@@ -3,7 +3,7 @@
 import { useRef } from "react";
 import { projectsData } from "@/lib/data";
 import Image from 'next/image';
-import { useScroll } from 'framer-motion';
+import { useScroll, useTransform } from 'framer-motion';
 import { motion } from "framer-motion";
 
 type ProjectProps = (typeof projectsData)[number];
@@ -13,9 +13,10 @@ ProjectProps) {
     const ref = useRef<HTMLDivElement>(null);
     const { scrollYProgress } = useScroll({
         target: ref,
-        offset: ["0 1", "1.33 1"]
+        offset: ["0 1", "1.33 1"],
     });
-
+    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
 
     return (
         <motion.div
@@ -25,12 +26,12 @@ ProjectProps) {
                 scale: scrollYProgress,
                 opacity: scrollYProgress,
             }} 
-            className="mb-3 sm:mb-8 last:mb-0"
+            className="group mb-3 sm:mb-8 last:mb-0"
         >
 
     <section 
-    className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden 
-    sm:pr-8 relative sm:h-[20rem] even:pl-8 hover:bg-gray-200 transition'>
+    className='bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden 
+    sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8'>
         <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] 
         flex flex-col h-full group-even:ml-[18rem]'>
             <h3 
