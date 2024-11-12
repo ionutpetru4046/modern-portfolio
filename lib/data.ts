bb274ace3243d78875d07ae79e80f9390f36cf6a
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-12 13:41:26 +0000

    framer-motion added to the div inside project component

--- lib/data.ts
+++ lib/data.ts
@@ -34,7 +34,7 @@ export const projectsData = [
     title: "The Ultimate SaaS Toolkit",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    tags: ["React.js", "Vite", "Tailwind CSS", "Framer-Motion"],
     imageUrl: project1,
   },
   {
