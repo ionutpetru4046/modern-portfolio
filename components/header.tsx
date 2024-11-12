1f0e896b089b95d28ffcf791f79a6fa96fbf81eb
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-12 19:14:56 +0000

    fixed header.tsx and active-section component

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,10 @@ import { motion } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from 'next/link';
 import clsx from 'clsx';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Header() {
+    const { activeSection, setActiveSection } = useActiveSectionContext();
   return (
     <header className='z-[999] relative'>
         <motion.div className='fixed top-0 left-1/2 h-[4.5rem] w-[full] rounded-none 
