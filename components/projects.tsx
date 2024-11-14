d958b74c840b490ac80a9d78e186dae33d2b503f
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-14 14:37:25 +0000

    skills component finished

--- components/projects.tsx
+++ components/projects.tsx
@@ -7,10 +7,10 @@ import Project from './project';
 import { useSectionInView } from '@/lib/hooks';
 
 export default function Projects() {
-  const { ref } = useSectionInView('Projects', 0.1);
+  const { ref } = useSectionInView('Projects', 0.2);
 
   return (
-    <section ref={ref} id='projects' className='scroll-mt-28'>
+    <section ref={ref} id='projects' className='scroll-mt-28 mb-28'>
         <SectionHeading>My Projects</SectionHeading>
         <div>
            {projectsData.map((project, index) => (
