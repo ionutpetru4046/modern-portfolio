ebcf1e8aed887e0897445cb1cf553f2a327e53d6
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-14 12:53:01 +0000

    threshold changed to projects component

--- components/projects.tsx
+++ components/projects.tsx
@@ -7,7 +7,7 @@ import Project from './project';
 import { useSectionInView } from '@/lib/hooks';
 
 export default function Projects() {
-  const { ref } = useSectionInView('Projects', 0.5);
+  const { ref } = useSectionInView('Projects', 0.1);
 
   return (
     <section ref={ref} id='projects' className='scroll-mt-28'>
@@ -17,7 +17,7 @@ export default function Projects() {
             <React.Fragment key={index}>
                 <Project {...project} />
             </React.Fragment>
-           ))} 
+           ))}
         </div>
     </section>
   );
