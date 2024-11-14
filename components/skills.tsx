e8d5275b5662a2b2b64ec820244fb8883181c15a
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-14 13:27:47 +0000

    skills added to the component mapping through array

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import SectionHeading from './section-heading'
+import { skillsData } from '@/lib/data'
 
 export default function Skills() {
   return (
     <section>
         <SectionHeading > My Skills</SectionHeading>
+        <ul>
+            {
+                skillsData.map((skills, index) => (
+                    <li key={index}>{skills}</li>
+                ))
+            }
+        </ul>
     </section>
   )
 }
