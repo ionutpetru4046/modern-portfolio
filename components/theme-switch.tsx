0d9f62103128a71a0a93c24970bc1778f43747c9
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-14 21:48:22 +0000

    customized theme button added and styles

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { BsSun } from 'react-icons/bs'
+"use client";
+
+import React, { useState } from 'react'
+import { BsMoon, BsSun } from 'react-icons/bs'
+
+type Theme = "light" | "Dark";
 
 export default function ThemeSwitch() {
+    const [theme, setTheme] = useState<Theme>("light");
+
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('Dark');
+        } else {
+            setTheme('light')
+        }
+    }
+
   return (
     <button className='fixed bottom-5 right-5 bg-white w-[3rem] 
     h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white 
     border-opacity-40 shadow-2xl rounded-full flex items-center justify-center 
-    hover:scale-[1.15] active:scale-105 transition-all'>
-      <BsSun />
+    hover:scale-[1.15] active:scale-105 transition-all' 
+    onClick={toggleTheme}
+    >
+      {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
   )
 }
