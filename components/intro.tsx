ce18766d0580bdf9ca0c2516f30ad29d23e6c078
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-19 09:07:13 +0000

    resume updated main CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -101,7 +101,7 @@ export default function Intro() {
         <a 
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer 
           dark:bg-white/10 border border-black/10" 
-          href="/Resume.pdf" download
+          href="/public/Resume updated.pdf" download
         >
           Download CV{" "} 
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
