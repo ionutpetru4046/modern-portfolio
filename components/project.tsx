90879c01da37191120a90920cbbb8dcf240a6592
Coste Ionut Petru <ionut4046@yahoo.com>
2024-11-28 18:52:21 +0000

    some changes added to the projects component

--- components/project.tsx
+++ components/project.tsx
@@ -48,23 +48,13 @@ export default function Project({
             ))}
           </ul>
         </div>
-        <a href={link} target="_blank" rel="noopener noreferrer">
+        <a href={link} target="_blank" rel="noopener noreferrer" className="absolute top-0 left-0 w-full h-full">
           <Image
             src={imageUrl}
             alt="Projects images"
-            quality={95}
-            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-          transition 
-          group-hover:scale-[1.04]
-          group-hover:-translate-x-3
-          group-hover:translate-y-3
-          group-hover:-rotate-2
-
-          group-even:group-hover:translate-x-3
-          group-even:group-hover:translate-y-3
-          group-even:group-hover:rotate-2
-
-          group-even:right-[initial] group-even:-left-40"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
           />
         </a>
       </section>
