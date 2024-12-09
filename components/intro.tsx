4165d837ffbd1ba1bffbd8c185fa2fae47560451
Coste Ionut Petru <ionut4046@yahoo.com>
2024-12-09 19:00:12 +0000

    changes to logo image

--- components/intro.tsx
+++ components/intro.tsx
@@ -34,7 +34,7 @@ export default function Intro() {
             }}
           >
             <Image 
-              src="/profile-picture.JPG"
+              src="/profile-picture.jpg"
               alt="logo"
               width="292"
               height="292"
