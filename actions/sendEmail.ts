f535b4e5df7e6714fc1b0b9cf4e7be17b956baec
ionutpetru4046 <ionut4046@yahoo.com>
2025-01-26 21:15:33 +0000

    some changes to portfolio

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,18 @@
 "use server";
 
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
     console.log("Running on the server");
     console.log(formData.get("senderEmail"));
     console.log(formData.get("message"));
-  }
+
+    resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "ionion4046@gmail.com",
+      subject: "Message from contact form",
+      text: "Hello World",
+    });
+  };
