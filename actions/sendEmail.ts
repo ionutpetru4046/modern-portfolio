eb2860fcfff099d88d7cae35f55fc96e3f0ad43f
ionutpetru4046 <ionut4046@yahoo.com>
2025-01-26 21:40:47 +0000

    server side validation statement created

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -4,15 +4,35 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const validateString = (value: unknown) => {
+  if (!value  || typeof value !== "string") {
+    return false;
+  }
+
+  return true;
+}
+
 export const sendEmail = async (formData: FormData) => {
-    console.log("Running on the server");
-    console.log(formData.get("senderEmail"));
-    console.log(formData.get("message"));
+  const senderEmail = formData.get("senderEmail");
+    const message = formData.get("message");
+
+    // simple server-side validation
+    if (!validateString(senderEmail)) {
+      return {
+        error: "Invalid sender email"
+      }
+    }
+    if (!validateString(message)) {
+      return {
+        error: "Invalid message"
+      }
+    }
 
     resend.emails.send({
       from: "onboarding@resend.dev",
       to: "ionion4046@gmail.com",
       subject: "Message from contact form",
-      text: "Hello World",
+      reply_to: senderEmail,
+      text: message,
     });
   };
