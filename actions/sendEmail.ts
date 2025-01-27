27e2d85d88d9361f05b32a410718a092de2b5fda
ionutpetru4046 <ionut4046@yahoo.com>
2025-01-27 21:29:44 +0000

    new component created

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,16 +1,9 @@
 "use server";
 
 import { Resend } from "resend";
+import { validateString } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-const validateString = (value: unknown, maxLength: number) => {
-  if (!value  || typeof value !== "string" || value.length > maxLength) {
-    return false;
-  }
-
-  return true;
-}
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
