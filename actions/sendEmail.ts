9890d143b597035184e97dac111582adb0b00e9e
ionutpetru4046 <ionut4046@yahoo.com>
2025-02-02 20:16:57 +0000

    working on sendEmail component

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,6 +5,22 @@ import { validateString } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const getErrorMessage = (error: unknown): string => {
+  let message: string;
+
+  if (error instanceof Error) {        
+    message = error.message;
+  } else if  (error && typeof error ==='object' && 'message' in error){
+    message = String(error.message);
+  } else if(typeof error === 'string') {
+    message = error;
+  } else {
+    message = "Unknown error";
+  }
+
+  return message;
+};
+
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
     const message = formData.get("message");
@@ -31,10 +47,8 @@ export const sendEmail = async (formData: FormData) => {
       });
       
     } catch (error: unknown) {
-      if (error instanceof Error) {        
-        return {
-          error: error.message,
-        };
+      return {
+        error: getErrorMessage(error),
       }
     }
   };
