d70c22efb09604737225448bdb63e782b02a980e
ionutpetru4046 <ionut4046@yahoo.com>
2025-02-02 20:21:22 +0000

    working on email components

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,25 +1,9 @@
 "use server";
 
 import { Resend } from "resend";
-import { validateString } from "@/lib/utils";
+import { validateString, getErrorMessage } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-const getErrorMessage = (error: unknown): string => {
-  let message: string;
-
-  if (error instanceof Error) {        
-    message = error.message;
-  } else if  (error && typeof error ==='object' && 'message' in error){
-    message = String(error.message);
-  } else if(typeof error === 'string') {
-    message = error;
-  } else {
-    message = "Unknown error";
-  }
-
-  return message;
-};
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
