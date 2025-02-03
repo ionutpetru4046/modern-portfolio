4004276fe8ecee24e73c699b267baa51afb16f60
ionutpetru4046 <ionut4046@yahoo.com>
2025-02-03 18:37:02 +0000

    submit btn component added to the project

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,13 +2,15 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
+import { useFormStatus } from "react-dom";
+import SubmitBtn from "./submit-btn";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
+  const { pending } = useFormStatus();
 
   return (
     <motion.section
@@ -59,15 +61,7 @@ export default function Contact() {
           required
           maxLength={5000}
         />
-        <button 
-        type="submit"
-        className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 
-        text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 dark:bg-white dark:bg-opacity-10 
-        hover:bg-gray-950"
-        >
-          Submit <FaPaperPlane className="text-xs opacity-70 transition-all 
-          group-hover:translate-x-1 group-hover:-translate-y-1" />{" "} 
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -22,9 +22,9 @@ export default function ContactFormEmail({ message, senderEmail }: ContactFormEm
     <Head />
     <Preview>New Message from your portfolio website</Preview>
     <Tailwind>
-      <Body className='bg-gray-100'>
+      <Body className='bg-gray-100 text-black'>
         <Container>
-          <Section className='bg-white border-black my-10 px-10 py-4 rounded-md'>
+          <Section className='bg-white borderBlack my-10 px-10 py-4 rounded-md'>
             <Heading className='leading-tight'>You received the following message from the contact form</Heading>
             <Text>{message}</Text>
             <Hr />
