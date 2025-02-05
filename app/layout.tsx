3a386245e4f617dc04a17a7a4ebe56fba55b46e3
ionutpetru4046 <ionut4046@yahoo.com>
2025-02-05 19:31:38 +0000

    more code to the project added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from 'next/font/google'
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import Footer from '@/components/footer'
 import ThemeSwitch from '@/components/theme-switch'
-// import { ThemeContextProvider } from '@/context/theme-context'
 import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -31,7 +30,6 @@ export default function RootLayout({
         w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] 
         xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]'></div>
 
-        {/* <ThemeContextProvider> */}
           <ActiveSectionContextProvider>
             <Header />
             {children}
@@ -39,7 +37,6 @@ export default function RootLayout({
             <Toaster position="top-right" />
             <ThemeSwitch />
           </ActiveSectionContextProvider>
-         {/* </ThemeContextProvider> */}
         </body>
     </html>
   )
